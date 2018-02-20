a8b908cb90d8b2e448463f12bba288e1badb0abc
FabianMuli <fabian.muli@students.jkuat.ac.ke>
2018-02-20 13:58:23 +0300

    chnageing password form

--- flaskr/forms.py
+++ flaskr/forms.py
@@ -5,17 +5,22 @@ from wtforms.validators import Required, Email, EqualTo, Length
 class LoginForm(Form):
     email =StringField(validators=[Email(), Required()])
     password = PasswordField(validators=[Required()])
-    submit = SubmitField("Submit")
+    submit = SubmitField("Log In")
     remember_me = BooleanField('remember me', default=True)
 
 class SignupForm(Form):
     first_name = StringField("First name:", validators=[Required()])
     second_name = StringField('Second name:', validators=[Required()])
-    email = StringField('Email:', validators=[Email(), Required(), Length(1, 64)])
+    email = StringField('Email:', validators=[Email(), Required()])
     mobile_number = IntegerField('Mobile number:', validators=[Required()])
     password = PasswordField('Password:', validators=[Required(), Length(8, 20)])
     password2 = PasswordField('Confirm password', validators=[EqualTo('password'), Required()])
     submit = SubmitField('Submit')
+
+class ChangePasswordForm(Form):
+    email = StringField(validators=[Email(), Required()])
+    submit = SubmitField('Reset my password')
+
 
 class PostForm(Form):
     post = StringField("Whats on your mind:", validators=[Required()])
