140cd33f4f2837db80e801c7688f7cce59b8824f
FabianMuli <fabian.muli@students.jkuat.ac.ke>
2018-03-16 12:02:53 +0300

    sed black for reformatting

--- flaskr/forms.py
+++ flaskr/forms.py
@@ -2,11 +2,13 @@ from flask_wtf import Form
 from wtforms import StringField, BooleanField, SubmitField, IntegerField, PasswordField, FileField
 from wtforms.validators import Required, Email, EqualTo, Length
 
+
 class LoginForm(Form):
-    email =StringField(validators=[Email(), Required()])
+    email = StringField(validators=[Email(), Required()])
     password = PasswordField(validators=[Required()])
     submit = SubmitField("Log In")
     remember_me = BooleanField('remember me', default=True)
+
 
 class SignupForm(Form):
     first_name = StringField("First name:", validators=[Required()])
@@ -14,8 +16,11 @@ class SignupForm(Form):
     email = StringField('Email:', validators=[Email(), Required()])
     mobile_number = IntegerField('Mobile number:', validators=[Required()])
     password = PasswordField('Password:', validators=[Required(), Length(8, 20)])
-    password2 = PasswordField('Confirm password', validators=[EqualTo('password'), Required()])
+    password2 = PasswordField(
+        'Confirm password', validators=[EqualTo('password'), Required()]
+    )
     submit = SubmitField('Submit')
+
 
 class ChangePasswordForm(Form):
     email = StringField(validators=[Email(), Required()])
@@ -26,6 +31,7 @@ class PostForm(Form):
     post = StringField("Whats on your mind:", validators=[Required()])
     submit = SubmitField("Post")
 
+
 class UploadPhoto(Form):
-    picture = FileField("Upload profile photo:",validators=[Required()])
+    picture = FileField("Upload profile photo:", validators=[Required()])
     upload = SubmitField("Upload")
